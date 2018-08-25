e07dc2334dbb07e3f102754c0213b151ff4229a6
Adeola Ojo <adeola.ojo@transferwise.com>
2018-08-25 19:56:54 +0100

    detached updates

--- synapse-config-client/src/main/java/com/synapse/io/config/client/App.java
+++ synapse-config-client/src/main/java/com/synapse/io/config/client/App.java
@@ -1,13 +1,11 @@
 package com.synapse.io.config.client;
 
-import com.synapse.io.util.CustomStringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println(new CustomStringUtil().toUpper("dexter"));
         SpringApplication.run(App.class, args);
     }
 }
